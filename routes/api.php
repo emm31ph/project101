9cbd908c65157576e32783f6aac2e6185d692e99
egyok <edmund.managuit@gmail.com>
2019-08-20 11:34:36 +0800

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Http\Request;
+ 
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['middleware' => ['json.response']], function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	Route::post('login', 'PassportController@login');
+	Route::post('register', 'PassportController@register');
+
+	Route::middleware('auth:api')->group(function () {
+	    Route::get('user', 'PassportController@details');
+
+	    // Route::resource('products', 'ProductController');
+	});
+
+// private routes
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/logout', 'PassportController@logout')->name('logout');
+    });
 });
+
